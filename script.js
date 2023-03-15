3a92a6657a20c59f6fb50700a068968846a6feae
Sujitsinh30 <sujitsinh1501@tnraocollege.org>
2023-03-15 23:25:53 +0530

    design for the quizz is completed

---
+++ script.js
@@ -0,0 +1,22 @@
+function gk()
+{
+    let btns = document.getElementById('btns').style.visibility="hidden";
+    console.log('1');          
+}
+function general()
+{
+    questions:
+    {
+        question1:{
+            question : "Cricket is the National sport of ________."
+            options:
+            [
+                            "INDIA",
+                            "AUSTRALIA",
+                            "CHINA",
+                            "USA"
+            ]
+            answer:"AUSTRALIA"
+        }
+    }
+}
